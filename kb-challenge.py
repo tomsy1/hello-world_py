f10bf831502d1828f51828219bd0abfeae5a9577
James Toms <jamestoms84@gmail.com>
2020-06-03 10:51:39 +0100

    General update
    Signed-off-by: James Toms <jamestoms84@gmail.com>

--- kb-challenge.py
+++ kb-challenge.py
@@ -1,28 +1,33 @@
 import datetime
+import pandas as pd
 dt = datetime.datetime.today()
-#Average number of swings needed a day to reach 10,000
 
 
+data = [
+    {"Date": "01/06/2020",  "Swings": 350},
+    {"Date": "02/06/2020",  "Swings": 370},
+    {"Date": "03/06/2020", "Swings": 100},
+]
+df = pd.DataFrame(data)
 
-swing_count = {
-    1:350,
-    2: 370,
-
-
-}
+df['Date'] = pd.to_datetime(df['Date'])
+max_swings = df['Swings'].max()
+max_swings_date = df.iloc[df['Swings'].idxmax()]
+total_swings = df['Swings'].sum()
 days_in_month = 30
 goal = 10000
 goal_formatted = format(10000, ",d")
 goal_average_daily_swings = round(goal / days_in_month)
-total_swings = sum(swing_count.values())
 current_day = dt.day
 run_rate = round((total_swings / current_day)*days_in_month)
 days_remaining = days_in_month - current_day
 run_rate_goal = (((run_rate-goal)/goal)*100)+100
 av_swings_day = (total_swings / current_day)
-print (av_swings_day)
 
 
+
+
+max_swings_date_formatted = (max_swings_date['Date'])
 
 print("=================================================")
 if run_rate_goal < 95.0:
@@ -36,6 +41,7 @@ print("Total swings so far this month: " + str(total_swings))
 print("Average swings a day: " + str(av_swings_day))
 print("Current run rate: " + str(run_rate))
 print ("The current run rate is " + str(run_rate_goal) + "% of goal")
+print ("The max number of swings in one day was: " + str(max_swings) + " which occured on " + max_swings_date_formatted.strftime("%A %d %B."))
 print("Days remaining: " + str(days_remaining))
 
 
